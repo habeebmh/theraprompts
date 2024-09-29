95dc796b615be1b2787b6ef2b42946e05fccebce
Habeeb Hooshmand <habeeb.hooshmand@gmail.com>
2024-09-28 22:40:58 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useMemo } from 'react';
+
+import prompts from './constants/prompts';
 
 function App() {
+
+  const selectedItem = useMemo(() => {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const index = (day + month + year) % prompts.length;
+    return prompts[index];
+  }, []);
+
+  const currentDate = useMemo(() => new Date().toLocaleDateString(), []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className='prompt-container'>
+          <h2 className="subtitle">Prompt #{selectedItem.index} </h2>
+          <h1 className="title">{selectedItem.text}</h1>
+          <h3 className="small-subtitle">{currentDate}</h3>
+          <div className="scroll-message">
+            Scroll down to see all prompts
+          </div>
+        </div>
+        <div className="prompt-list">
+          {prompts.map((prompt, index) => (
+            <div key={index} className="prompt-item">
+              <span className="prompt-index">#{prompt.index}</span>
+              <span className="prompt-text">{prompt.text}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
