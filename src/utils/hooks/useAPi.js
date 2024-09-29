8c957eb1ead4627b12a6da938f926a61263fe5e4
Habeeb Hooshmand <habeeb.hooshmand@gmail.com>
2024-09-29 17:26:04 -0500

    Add firebase and react-use-promise dependencies

---
+++ src/utils/hooks/useAPi.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import usePromise from 'react-use-promise'
+
+export const useApi = (apiPromise, deps) => {
+  const [triggerState, setTriggerState] = useState(false)
+  const [result, error, state] = usePromise(apiPromise, [...deps, triggerState])
+
+  function trigger () {
+    setTriggerState(prev => !prev)
+  }
+
+  return { data: result, error, loading: state === 'pending', trigger }
+}
